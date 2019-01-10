c2020c265a2d464ea4c552f6b76818a01f6d2c61
Amanda Lemmons <amanda.lemmons@gmail.com>
2019-01-10 18:32:18 +0100

    implemented user connection to posts with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@test.com", password: "12345678", password_confirmation: "12345678", first_name: "John", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -23,14 +25,19 @@ describe 'navigate' do
     end
 
     it 'can be created from new form page' do
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
 
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User_Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User_Association")
     end
   end
 end
