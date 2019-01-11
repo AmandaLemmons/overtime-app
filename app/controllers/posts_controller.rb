72c5baec88b20434a169647ee4afd1920f08839c
Amanda Lemmons <amanda.lemmons@gmail.com>
2019-01-11 14:09:59 +0100

    Add delete method to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -27,11 +27,17 @@ class PostsController < ApplicationController
     if @post.update(post_params)
       redirect_to @post, notice: "Your post was edited successfully"
     else
-      render :edit 
+      render :edit
     end
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: "Your post was deleted successfully"
+
   end
 
   private
